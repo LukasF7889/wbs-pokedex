ecf6c096bc1203b32a5f0edbf1ac6c9a61550e2f
LukasF7889 <lukas_fritsch@hotmail.de>
2025-03-25 15:57:41 +0100

    Margin fix

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 const Layout = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Pokemon!!!!</h1>
+      <h1 className="text-3xl font-bold underline pb-4">Pokemon!!!!</h1>
       <Outlet />
     </div>
   );
